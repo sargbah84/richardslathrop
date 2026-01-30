9a3b2db5b8ebf3e48a09d3245e2038f69b71f753
Prince Sargbah <sargbah84@gmail.com>
2026-01-30 11:58:15 -0500

    refactor: remove AccessProvider wrapper from Providers component

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import { AccessProvider } from "./AccessGate";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <AccessProvider>{children}</AccessProvider>;
+  return <>{children}</>;
 }
